c4f430120c4485723208d1880a343e494064920e
Michal Donat <michal@donat.cz>
2020-07-11 17:54:47 +0200

    allow removing hardlinks, movies_tree.py prints stats on exit

--- movies_tree.py
+++ movies_tree.py
@@ -5,6 +5,7 @@ import csv
 import os
 import shutil
 import sys
+from collections import defaultdict
 from pathlib import Path
 
 from lib.cmdline import OpenInputFileAction, StoreColumnsListAction, EnsureDirectoryAction, \
@@ -35,7 +36,8 @@ class Program:
         -g comma-separated list of group-by columns
         -d directory containing source media files
         -o new tree directory (on same filesystem as the source dir)
-        -x clear new tree directory before creating new tree (not clearing CWD)
+        -u update directory tree (allow removing hard links)
+        -r clear new tree directory before creating new tree (not clearing CWD)
         """
         parser = argparse.ArgumentParser(description="Creates groupped movie directories.")
 
@@ -70,21 +72,32 @@ class Program:
                             action=EnsureDirectoryAction, dest='output_dir', metavar='DIRECTORY',
                             default=Path('.'), help="Directory for output hardlinks. Will be created if not exist.")
 
-        parser.add_argument('-x',  # CLEAR OUTPUT DIRECTORY
+        parser.add_argument('-u',  # UPDATE OUTPUT_DIRECTORY
+                            action='store_true', dest='output_update',
+                            help="Allow removing hardlinks in the output tree."
+                            )
+
+        parser.add_argument('-r',  # CLEAR OUTPUT DIRECTORY
                             action='store_true', dest='output_clear',
                             help="Clear all files in the output directory before creating new hardlinks. By default "
                                  "clearing of the current working directory is prohibited.")
 
+        parser.add_argument('-v',  # VERBOSE
+                            action='store_true', dest='verbose',
+                            help='Print what is done.')
         self.args = parser.parse_args()
+
+        self.stats = defaultdict(int)
 
     def main(self):
         output_is_cwd = self.args.output_dir.samefile('.')
         if self.args.output_clear and not output_is_cwd:
             shutil.rmtree(self.args.output_dir)
+            self.stats['clear_output_dir'] += 1
             print('Removed all contents of the target directory: {0}/'.format(self.args.output_dir.absolute()))
 
         reader = csv.reader(self.args.input, delimiter=",", quotechar='"')
-        next(reader)  # skip header row. TODO: separate command-line argument?
+        next(reader)  # skip header row.
         for line in reader:
             original_filename = line[FILENAME_COLUMN_ID]
             _, fn_extension = os.path.splitext(original_filename)
@@ -92,6 +105,7 @@ class Program:
 
             if not source_path.is_file():
                 print('Source movie file not found: ' + str(source_path.absolute()))
+                self.stats['file_not_found'] += 1
                 continue
 
             movie = self.parse_csv_movie(line)
@@ -118,12 +132,22 @@ class Program:
                         target_path.parent.mkdir(mode=0o755, parents=True, exist_ok=True)
 
                         if target_path.is_dir():
-                            print('Target path is a directory, I will not touch that: ' + str(target_path.absolute()))
+                            self.stats['hardlinks_are_dirs'] += 1
+                            if self.args.verbose:
+                                print(
+                                    'Target path is a directory, I will not touch that: ' + str(target_path.absolute()))
                             continue
 
                         if target_path.is_file():
                             if target_path.samefile(source_path):
+                                self.stats['hardlinks_found'] += 1
                                 continue  # be quiet when nothing is needed to be done
+
+                            if self.args.output_update:
+                                target_path.unlink()
+                                self.stats['hardlinks_removed'] += 1
+                                if self.args.verbose:
+                                    print("Removed hardlink: " + target_path.absolute())
 
                         try:
                             if hasattr(source_path, 'link_to'):
@@ -131,11 +155,15 @@ class Program:
                             else:  # python<3.8
                                 os.link(str(source_path.absolute()), str(target_path.absolute()))
 
-                            print("Created new hardlink: '{0}' -> '{1}'".format(source_path, target_path))
+                            self.stats['hardlinks_new'] += 1
+                            if self.args.verbose:
+                                print("Created new hardlink: '{0}' -> '{1}'".format(source_path, target_path))
 
                         except FileExistsError:
-                            print("I would like to overwrite, I will not touch that: {0}."
-                                  .format(target_path.absolute()))
+                            self.stats['hardlinks_occupied'] += 1
+                            if self.args.verbose:
+                                print("I would like to overwrite, I will not touch that: {0}."
+                                      .format(target_path.absolute()))
                             # continue
 
     def parse_csv_movie(self, line):
@@ -171,6 +199,13 @@ class Program:
         fn = '{0} ({1})'.format(title, details) if details else title
         return fn.replace(r'/', '_')
 
+    def print_stats(self):
+        row_format = "|{:>15}" * 2 + '|'
+        print(row_format.format("event", "triggered"))
+        print(('|' + '-' * 15) * 2 + '|')
+        for item in self.stats.items():
+            print(row_format.format(*item))
+
 
 if __name__ == "__main__":
     program = Program()
@@ -180,3 +215,5 @@ if __name__ == "__main__":
 
     except KeyboardInterrupt:
         print()  # quiet Ctrl+C interruption, just finish last line.
+
+    program.print_stats()
